3a42ef34e5b21bf61c0cbc6e07b653f39c5b4675
Shane Hickman <srhickma@edu.uwaterloo.ca>
2019-05-27 01:47:01 -0400

    Add logger and config parsing

--- server.go
+++ server.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
 
+	"github.com/google/logger"
+	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
 	"github.com/urfave/negroni"
 )
 
 func main() {
-	router := mux.NewRouter()
+	loadConfig()
 
 	objectMap := make(map[string][]byte)
+
+	router := mux.NewRouter()
 
 	router.HandleFunc("/d/{oid}", func(w http.ResponseWriter, req *http.Request) {
 		params := mux.Vars(req)
@@ -19,6 +23,7 @@ func main() {
 
 		_, err := w.Write(objectMap[oid])
 		if err != nil {
+			logger.Errorf("Failed to write object response: %v \n", err)
 			panic(err)
 		}
 	}).Methods("GET")
@@ -29,6 +34,7 @@ func main() {
 
 		bytes, err := ioutil.ReadAll(req.Body)
 		if err != nil {
+			logger.Warningf("Failed to read object body: %v \n", err)
 			panic(err)
 		}
 
@@ -38,5 +44,27 @@ func main() {
 	n := negroni.Classic()
 	n.UseHandler(router)
 
-	_ = http.ListenAndServe(":4444", n)
+	addr := viper.GetString("addr")
+	_ = http.ListenAndServe(addr, n)
+}
+
+func loadConfig() {
+	viper.SetConfigName("conf")
+
+	viper.AddConfigPath("/etc/dead-drop/")
+	viper.AddConfigPath("$HOME/.dead-drop/")
+	viper.AddConfigPath(".")
+
+	viper.SetDefault("addr", ":4444")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		switch err.(type) {
+		case viper.ConfigFileNotFoundError:
+			logger.Info("No config file found, using the default configuration")
+			break
+		default:
+			logger.Warningf("Failed to load config file: %v \n", err)
+		}
+	}
 }
