814c5f326fb3cdf1ddf381e11262be5e8c3e3b9e
Shane Hickman <srhickma@edu.uwaterloo.ca>
2019-07-14 18:45:48 -0400

    Set key name regex

--- lib/common.go
+++ lib/common.go
@@ -3,7 +3,7 @@ package lib
 const DefaultConfigDir = ".dead-drop"
 const DefaultConfigName = "conf"
 const TokenCipherLabel = "token"
-const KeyNameRegex = ""
+const KeyNameRegex = "^[a-zA-Z0-9_-]{1,64}$"
 
 type TokenRequestPayload struct {
 	KeyName string
