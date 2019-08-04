3e10f51081bfa049a846afeeff82fe9f3576aea2
Shane Hickman <srhickma@edu.uwaterloo.ca>
2019-08-04 01:20:56 -0700

    Guard encryption keys in memory (#39)
    * Guard encryption keys in memory
    
    * Guard memory during key splitting

--- client/encryption.go
+++ client/encryption.go
@@ -4,67 +4,86 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/hmac"
-	"crypto/rand"
 	"crypto/sha256"
+	"dead-drop/client/ghash"
 	"fmt"
-	"io"
+	"github.com/awnumar/memguard"
 )
 
 const ivLength = aes.BlockSize
 
-func encrypt(key []byte, data []byte) ([]byte, error) {
+func encrypt(key *memguard.LockedBuffer, data []byte) ([]byte, error) {
 	encryptionKey, hmacKey := splitKeyHash(key)
 
-	block, err := aes.NewCipher(encryptionKey)
+	block, err := aes.NewCipher(encryptionKey.Bytes())
 	if err != nil {
 		return nil, err
 	}
 
-	ciphertext := make([]byte, ivLength+len(data))
-	iv := ciphertext[:ivLength]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
+	ciphertext := memguard.NewBufferRandom(ivLength + len(data))
+	defer ciphertext.Destroy()
 
+	iv := ciphertext.Bytes()[:ivLength]
 	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(ciphertext[ivLength:], data)
 
-	hash := hmac.New(sha256.New, []byte(hmacKey))
-	hash.Write([]byte(ciphertext))
+	ciphertext.Melt()
+	stream.XORKeyStream(ciphertext.Bytes()[ivLength:], data)
+	ciphertext.Freeze()
+
+	encryptionKey.Destroy()
+
+	hash := hmac.New(sha256.New, hmacKey.Bytes())
+	hash.Write(ciphertext.Bytes())
 	signature := hash.Sum(nil)
+	hmacKey.Destroy()
 
-	message := append(signature, ciphertext...)
+	message := append(signature, ciphertext.Bytes()...)
 	return message, nil
 }
 
-func decrypt(key []byte, message []byte) ([]byte, error) {
+func decrypt(key *memguard.LockedBuffer, message []byte) (*memguard.LockedBuffer, error) {
 	encryptionKey, hmacKey := splitKeyHash(key)
 
 	signature := message[:sha256.Size]
 	ciphertext := message[sha256.Size:]
 
-	hash := hmac.New(sha256.New, []byte(hmacKey))
+	hash := hmac.New(sha256.New, hmacKey.Bytes())
 	hash.Write([]byte(ciphertext))
 	expectedSignature := hash.Sum(nil)
+	hmacKey.Destroy()
 
 	if !hmac.Equal(signature, expectedSignature) {
 		return nil, fmt.Errorf("bad signature")
 	}
 
-	block, err := aes.NewCipher(encryptionKey)
+	block, err := aes.NewCipher(encryptionKey.Bytes())
 	if err != nil {
 		return nil, err
 	}
 
-	data := make([]byte, len(ciphertext)-ivLength)
+	data := memguard.NewBuffer(len(ciphertext) - ivLength)
+
 	iv := ciphertext[:ivLength]
 	stream := cipher.NewCTR(block, iv)
-	stream.XORKeyStream(data, ciphertext[ivLength:])
+
+	data.Melt()
+	stream.XORKeyStream(data.Bytes(), ciphertext[ivLength:])
+	data.Freeze()
+
+	encryptionKey.Destroy()
 
 	return data, nil
 }
 
-func splitKeyHash(key []byte) ([]byte, []byte) {
-	sum := sha256.Sum256(key)
-	return sum[:16], sum[16:]
+func splitKeyHash(keyBuf *memguard.LockedBuffer) (*memguard.LockedBuffer, *memguard.LockedBuffer) {
+	sum := ghash.Sum256(keyBuf)
+	defer sum.Destroy()
+
+	keyBuf.Destroy()
+
+	sum.Melt()
+	key1 := memguard.NewBufferFromBytes(sum.Bytes()[:16])
+	key2 := memguard.NewBufferFromBytes(sum.Bytes()[16:])
+
+	return key1, key2
 }
