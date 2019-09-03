c4a7bddc5c71dc4516f8a526b1fb834dbf9b55bf
Shane Hickman <srhickma@edu.uwaterloo.ca>
2019-09-03 18:07:48 -0400

    Add test stubs and setup travis-ci (#44)
    * Fix makefile for running test
    
    * Add stubs for database tests
    
    * Add integration test stubs and more db tests
    
    * Use travis-ci to build and test
    
    * Properly install go-bindata in travis

---
+++ integration/integration_test.go
@@ -0,0 +1,77 @@
+package integration
+
+import (
+	"testing"
+)
+
+func TestSimpleDropPull(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestMultiDropPull(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestFailedAuthentication(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestFailedChecksum(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestObjectNotFound(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestPullExpiredObject(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestClientSideEncryption(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestGenerateKey(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestAddKey(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestKeyNameEnforcement(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestTLSCertificateVerification(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestLargeObjects(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestManyObjects(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestConcurrentClients(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestFileBasedConfiguration(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestInvalidRemote(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestHttpProtocol(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestReadAcrossSecretRotationBoundary(t *testing.T) {
+	// TODO(shane)
+}
---
+++ server/db_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSimpleDropPull(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestIndexExistingObjects(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestObjectExpiration(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestDestructiveRead(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestHeapCleanerWithConcurrentPulls(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestHeapCleanerWithConcurrentDrops(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestHeapCleanWithExpiry(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestDbLockingStress(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestConcurrentReadAndExpiry(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestConcurrentReadAndRemoval(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestStaleObjectMap(t *testing.T) {
+	// TODO(shane)
+}
+
