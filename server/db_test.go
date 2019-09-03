b929d20189af08a7902b25bd21c3ff81e6b95bf9
Shane Hickman <srhickma@edu.uwaterloo.ca>
2019-09-03 15:37:19 -0400

    Add stubs for database tests

---
+++ server/db_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSimpleDropPull(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestIndexExistingObjects(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestObjectExpiration(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestDestructiveRead(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestHeapCleanerWithConcurrentPulls(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestHeapCleanerWithConcurrentDrops(t *testing.T) {
+	// TODO(shane)
+}
+
+func TestHeapCleanWithExpiry(t *testing.T) {
+	// TODO(shane)
+}
